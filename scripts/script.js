5b23a99e80a4f13eee8e60076a2a6c17db4e1021
Ben <pauleybj@gmail.com>
2021-01-29 12:25:58 +0000

    tested new APIs to be used

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,10 @@ $(document).ready(function () {
 
   $(document).on("click", ".recent-search", getMovieClicked);
 
+  function getMovieClicked() {
+    getMovieDetails($(this).attr("movie-name"));
+  }
+
   function storeRecentSearches(recentSearches) {
     var movie = $("#movie-input").val().trim();
 
@@ -58,10 +62,6 @@ $(document).ready(function () {
     }
   }
 
-  function getMovieClicked() {
-    getMovieDetails($(this).attr("movie-name"));
-  }
-
   function getMovieDetails(movie) {
     var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
 
@@ -69,11 +69,55 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET",
     }).then(function (response) {
-      console.log(response);
-
-      $("#main-film-poster").attr("src", response.Poster);
-      $("#main-film-name").text(response.Title + " (" + response.Year + ")");
-      $("#main-film-synopsis").text(response.Plot);
+      renderMainMovie(response);
     });
+  }
+
+  // getCelebrityInfo("Steven Spielberg");
+
+  function getCelebrityInfo(name) {
+    var apiKey = "Wvx0+onLZFq2287mLWm4CA==38WLOWdjk3UqQ6FZ";
+    var queryURL =
+      "https://api.celebrityninjas.com/v1/search?limit=1&name=" + name;
+
+    $.ajax({
+      method: "GET",
+      url: queryURL,
+      headers: { "X-Api-Key": apiKey },
+      contentType: "application/json",
+      success: function (response) {
+        console.log(response[0]);
+      },
+      error: function ajaxError(jqXHR) {
+        console.error("Error: ", jqXHR.responseText);
+      },
+    });
+  }
+
+  // getSimilarMovies("Frozen");
+
+  function getSimilarMovies(movie) {
+    $.ajax({
+      type: "GET",
+      url: "https://tastedive.com/api/similar?limit=10",
+      jsonp: "callback",
+      dataType: "jsonp",
+      data: {
+        type: "movie",
+        q: movie,
+        k: "400900-Popcornp-N9NY6GRY",
+      },
+      success: function (response) {
+        console.log(response.Similar.Results[0]);
+      },
+    });
+  }
+
+  function renderMainMovie(response) {
+    $("#body-container").css("display", "block");
+
+    $("#main-film-poster").attr("src", response.Poster);
+    $("#main-film-name").text(response.Title + " (" + response.Year + ")");
+    $("#main-film-synopsis").text(response.Plot);
   }
 });
